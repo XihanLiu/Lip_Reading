741118ded3c594be9d8f6de09ba34e7c999f4232
XihanLiu <xihan.liu04@gmail.com>
2022-12-01 19:16:34 -0500

    With dataset demonstration

--- dataset.py
+++ dataset.py
@@ -6,7 +6,6 @@ from PIL import Image
 import csv
 
 single_video_frames_dir = './example_lip_extracted/VideoImageExample/Lip_frameByFrame/'
-# frames_list = natsort.natsorted(os.listdir(single_video_frames_dir))
 
 class LipDataset(Dataset):
     def __init__(self,img_dir):
@@ -23,24 +22,53 @@ class LipDataset(Dataset):
             length = len(labels_dir)
             for label in range(1,length):
                 name = os.path.splitext(labels_dir[label])[0]
-                writer =  csv.writer(f)
+                writer = csv.writer(f)
                 writer.writerow([name])
             
         f.close()
         return Label_path 
     
-    def getitems(self, index):
+    def __len__(self):
+        with open(self.__getlabels__(),'r') as f:
+            lines = f.readlines()
+            length = len(lines)
+        f.close()
+        return length
+    
+    def size(self):
+        return self.__len__()
+
+    def getnames(self, index):
         """
-        Input: Index of label in the labels.
-        Return: Frames of image with label.
+        Input:      Index of label in the labels.
+        Return:     Frame names of video with label and num of frames.
         """
         with open(self.__getlabels__(),'r') as f:
             contents = f.readlines()
             label = (contents[index])[0:-1]
-            images = os.listdir(os.path.join(self.img_dir,label+self.surfix))
-            
+            images_names = os.listdir(os.path.join(self.img_dir,label+self.surfix))
+            num_images = len(images_names)
+            for index in range(len(images_names)):
+                images_names[index] = os.path.join(self.img_dir,label+self.surfix,images_names[index])
         f.close()
-        return (images, label)
+        return images_names,label,num_images
+
+    def getitems(self,index_group, index_single_frame):
+        """
+        Input:      Index of group of frames.
+        Return:     tensor of one frame with label.
+        """
+        convert_tensor = transforms.ToTensor()
+        frames, label, num_frames = self.getnames(index_group)
+        select_frame = Image.open(frames[index_single_frame])
+        tensor = convert_tensor(select_frame)
+        return tensor,label
+
 Output = LipDataset(single_video_frames_dir)
-Frames = Output.getitems(0)
-print(Frames)
+Size = Output.size()
+
+for group in range(Size):
+    names,label,length = Output.getnames(group)
+    for frame in range(length):
+        train_tensor,train_label = Output.getitems(group,frame)
+        
