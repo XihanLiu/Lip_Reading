5d56dd1b9668a20144cd22ffb0f80339497d629e
XihanLiu <xihan.liu04@gmail.com>
2022-12-02 13:37:00 -0500

    Renew Dataset.py with training path.

--- dataset.py
+++ dataset.py
@@ -4,23 +4,24 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
 import csv
+import natsort
 
-single_video_frames_dir = './example_lip_extracted/VideoImageExample/Lip_frameByFrame/'
+single_video_frames_dir = './Data/Lip_frameByFrame/'
 
 class LipDataset(Dataset):
     def __init__(self,img_dir):
         self.img_dir = img_dir
-        self.surfix = '.mp4/standard/typeI/'
+        self.surfix = '/standard/typeI/'
 
     def __getlabels__(self):
         """
         Create a single label file.
         """
-        Label_path = './example_lip_extracted/VideoImageExample/Labels.csv'
+        Label_path = "./Data/Labels.txt"
         with open(Label_path,'w') as f:
-            labels_dir = os.listdir(self.img_dir)
+            labels_dir = natsort.natsorted(os.listdir(self.img_dir))
             length = len(labels_dir)
-            for label in range(1,length):
+            for label in range(length):
                 name = os.path.splitext(labels_dir[label])[0]
                 writer = csv.writer(f)
                 writer.writerow([name])
@@ -34,7 +35,7 @@ class LipDataset(Dataset):
             length = len(lines)
         f.close()
         return length
-    
+
     def size(self):
         return self.__len__()
 
@@ -46,10 +47,12 @@ class LipDataset(Dataset):
         with open(self.__getlabels__(),'r') as f:
             contents = f.readlines()
             label = (contents[index])[0:-1]
-            images_names = os.listdir(os.path.join(self.img_dir,label+self.surfix))
-            num_images = len(images_names)
-            for index in range(len(images_names)):
-                images_names[index] = os.path.join(self.img_dir,label+self.surfix,images_names[index])
+            video_names = os.listdir(os.path.join(self.img_dir,label))
+            for video in video_names:
+                images_names = os.listdir(os.path.join(self.img_dir,label,video+self.surfix))
+                num_images = len(images_names)
+                for index in range(len(images_names)):
+                    images_names[index] = os.path.join(self.img_dir,label,video+self.surfix,images_names[index])
         f.close()
         return images_names,label,num_images
 
@@ -71,4 +74,5 @@ for group in range(Size):
     names,label,length = Output.getnames(group)
     for frame in range(length):
         train_tensor,train_label = Output.getitems(group,frame)
+        #print(train_tensor.shape,train_label) # Check output: "torch.Size([3, 50, 100]) AROUND"
         
